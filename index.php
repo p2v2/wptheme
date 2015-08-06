8e46ae085a112892e97027255995643d2b91848d
p2v2 <ppowar@ppowar-mn1.linkedin.biz>
2015-08-06 16:17:23 -0700

    Some sidebar stuff

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 									<p class="byline vcard">
 										<?php printf( __( 'Posted', 'bonestheme' ) . ' <time class="updated" datetime="%1$s" pubdate>%2$s</time> ' . __('by', 'bonestheme' ) . ' <span class="author">%3$s</span>', get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
-									<?php printf( '<p class="header-category"><span class="triangle"></span>' . __('filed under', 'bonestheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
+									<?php printf( '<p class="header-category"><span class="cat-list">' . __('filed under', 'bonestheme' ) . ': %1$s</span></p><span class="triangle"></span>' , get_the_category_list(', ') ); ?>
 
                   <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
 
--- sidebar.php
+++ sidebar.php
@@ -4,6 +4,16 @@
 
 		<?php dynamic_sidebar( 'sidebar1' ); ?>
 
+		<div class="widget badges">
+	    <h2 class="widget-header">Making your shopping easy</h2>
+	    <a href='http://www.ebates.com/rf.do?referrerid=4G6sn%2FOAxiTD1BWHKGOU5A%3D%3D' target='_blank' rel='nofollow'><img src='http://www.ebates.com/referral/2012/global_files/images/ebates_logo.png' alt='Ebates Coupons and Cash Back' height='31' width='171' border='0'/></a>
+	  </div>
+
+		<div class="widget badges">
+			<h2 class="widget-header">Featured:</h2>
+			<a class="centered" id="roadkill-rescue" href="http://roadkillrescue.net/2012/02/tree-stump-table/"><img src="http://roadkillrescue.net/buttonImages/Rescuer.png" target="_blank"/></a>
+		</div>
+
 	<?php else : ?>
 
 		<?php
